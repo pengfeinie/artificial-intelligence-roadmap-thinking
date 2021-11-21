267a061785582e18981f5f210c83a5ab09d986a8
pfnie <pfnie@yahoo.com>
2021-11-21 18:07:29 +0800

    d

--- paper/2014-VGG/vgg_16_keras.py
+++ paper/2014-VGG/vgg_16_keras.py
@@ -1,73 +1,116 @@
-import cv2
+"""
+VGG-16 Implementation on Cats&Dogs Dataset
+"""
+
+import keras
+import matplotlib.pyplot as plt
 import numpy as np
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
-from keras.layers.core import Flatten, Dense, Dropout
-from keras.models import Sequential
-from keras.optimizer_v1 import SGD
+import tensorflow as tf
+from keras.backend import set_session
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.layers import Conv2D, Dense, Flatten, MaxPool2D
+from keras.models import Sequential, load_model
+from keras.optimizer_v1 import Adam
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
 
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+config.log_device_placement = True  # to log device placement (on which device the operation ran)
+sess = tf.Session(config=config)
+set_session(sess)
 
-def VGG_16(weights_path=None):
-    model = Sequential()
-    model.add(ZeroPadding2D((1, 1), input_shape=(3, 224, 224)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+# Get the data
+trdata = ImageDataGenerator()
+traindata = trdata.flow_from_directory(directory="../Datasets/Cats&Dogs/train",target_size=(224,224))
+tsdata = ImageDataGenerator()
+testdata = tsdata.flow_from_directory(directory="../Datasets/Cats&Dogs/validation", target_size=(224,224))
 
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(128, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+# Generate the model
+model = Sequential()
+# Layer 1: Convolutional
+model.add(Conv2D(input_shape=(224, 224, 3), filters=64, kernel_size=(3, 3),
+                 padding='same', activation='relu'))
+# Layer 2: Convolutional
+model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 3: MaxPooling
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(256, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+# Layer 4: Convolutional
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 5: Convolutional
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 6: MaxPooling
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+# Layer 7: Convolutional
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 8: Convolutional
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 9: Convolutional
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 10: MaxPooling
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+# Layer 11: Convolutional
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 12: Convolutional
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 13: Convolutional
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 14: MaxPooling
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(Flatten())
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(4096, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1000, activation='softmax'))
+# Layer 15: Convolutional
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 16: Convolutional
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 17: Convolutional
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding='same', activation='relu'))
+# Layer 18: MaxPooling
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-    if weights_path:
-        model.load_weights(weights_path)
+# Layer 19: Flatten
+model.add(Flatten())
+# Layer 20: Fully Connected Layer
+model.add(Dense(units=4096, activation='relu'))
+# Layer 21: Fully Connected Layer
+model.add(Dense(units=4096, activation='relu'))
+# Layer 22: Softmax Layer
+model.add(Dense(units=2, activation='softmax'))
 
-    return model
+# Add Optimizer and check accuracy metrics
+optimizer = Adam(learning_rate=0.001)
+model.compile(optimizer=optimizer, loss=keras.losses.categorical_crossentropy,
+              metrics=['accuracy'])
+# Check model summary
+print(model.summary())
 
+checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True,
+                             save_weights_only=False, mode='auto', period=1)
+earlystop = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=1, mode='auto')
+hist = model.fit_generator(steps_per_epoch=100, generator=traindata, validation_data=testdata,
+                           validation_steps=10, epochs=100,
+                           callbacks=[checkpoint, earlystop])
 
-if __name__ == "__main__":
-    im = cv2.resize(cv2.imread('cat.jpg'), (224, 224)).astype(np.float32)
-    im[:, :, 0] -= 103.939
-    im[:, :, 1] -= 116.779
-    im[:, :, 2] -= 123.68
-    im = im.transpose((2, 0, 1))
-    im = np.expand_dims(im, axis=0)
+plt.plot(hist.history["acc"])
+plt.plot(hist.history['val_acc'])
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title("model accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("Epoch")
+plt.legend(["Accuracy", "Validation Accuracy","loss","Validation Loss"])
+plt.show(block=True)
 
-    # Test pretrained model
-    model = VGG_16('vgg16_weights.h5')
-    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy')
-    out = model.predict(im)
-    print(np.argmax(out))
+# Try on test data
+img = image.load_img("../Datasets/Cats&Dogs/test1/39.jpg",target_size=(224,224))
+img = np.asarray(img)
+plt.imshow(img)
+img = np.expand_dims(img, axis=0)
+saved_model = load_model("vgg16_1.h5")
+output = saved_model.predict(img)
+if output[0][0] > output[0][1]:
+    print("cat")
+else:
+    print('dog')
